11430ea8cc327db8b943802612a70c6f215fffa9
Prince Nana  Mensah <burnszephyr520@gmail.com>
2026-02-24 07:39:16 +0000

    Implement hello function to greet user by name

--- hellorust/src/main.rs
+++ hellorust/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+
+fn hello() {
+    let mut name: String = String::new();
+    io::stdin().read_line(&mut name).expect("Failed to read name");
+    if name.trim() == "" {
+        println!("Please enter your name:");
+        return;
+    }
+    println!("Hello, {}!", name.trim());
 }
+fn main() {
+    hello();
+}
+
